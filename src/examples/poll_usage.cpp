8e082b8d2576bdce8a5db07a87d92973eecccb84
remy <rde-brui@stduent.codam.nl>
2025-05-12 13:57:14 +0200

    mac to codam pc

--- src/examples/poll_usage.cpp
+++ src/examples/poll_usage.cpp
@@ -1,111 +1,111 @@
-#include <Webserv.hpp>
+// #include <Webserv.hpp>
 
-#include <unistd.h>	// read
-#include <string.h>    // for strncmp
-#include <sys/poll.h>
-#include <sys/epoll.h>
-#include <iostream>
-#include <stdio.h>
+// #include <unistd.h>	// read
+// #include <string.h>    // for strncmp
+// #include <sys/poll.h>
+// #include <sys/epoll.h>
+// #include <iostream>
+// #include <stdio.h>
 
-#define TIMEOUT 5
+// #define TIMEOUT 5
 
-/**
- * poll only exists to check events on the fd that you connected them with, e.g.
- * fd STDIN event POLLIN
- */
-void	poll_usages(void)
-{
-	struct pollfd fds[2];
+// /**
+//  * poll only exists to check events on the fd that you connected them with, e.g.
+//  * fd STDIN event POLLIN
+//  */
+// void	poll_usages(void)
+// {
+// 	struct pollfd fds[2];
 
-	/* Watch stdin for input */
-	// fds[0].fd = open("sam.txt", O_RDONLY, O_WRONLY, O_APPEND);
-	fds[0].fd = STDIN_FILENO;
-	fds[0].events = POLLIN;
+// 	/* Watch stdin for input */
+// 	// fds[0].fd = open("sam.txt", O_RDONLY, O_WRONLY, O_APPEND);
+// 	fds[0].fd = STDIN_FILENO;
+// 	fds[0].events = POLLIN;
 
-	/* Watch stdout for ability to write */
-	fds[1].fd = STDOUT_FILENO;
-	fds[1].events = POLLOUT;
+// 	/* Watch stdout for ability to write */
+// 	fds[1].fd = STDOUT_FILENO;
+// 	fds[1].events = POLLOUT;
 
-	//	created 2 fd
-	nfds_t nfds = 2;
-	while (true) {
-		// int ret = poll(fds, 1, TIMEOUT * 1000); // Wait for events
-		int ret = poll(fds, nfds, TIMEOUT * 1000); // Wait for events
+// 	//	created 2 fd
+// 	nfds_t nfds = 2;
+// 	while (true) {
+// 		// int ret = poll(fds, 1, TIMEOUT * 1000); // Wait for events
+// 		int ret = poll(fds, nfds, TIMEOUT * 1000); // Wait for events
 
-		if (ret == -1) {
-			perror("poll");
-			return ;
-		}
+// 		if (ret == -1) {
+// 			perror("poll");
+// 			return ;
+// 		}
 
-		if (ret == 0) {
-			std::cout << TIMEOUT << " seconds elapsed. No events detected." << std::endl;
-			// std::cerr << "ever here?" << std::endl;
-			continue;
-		}
+// 		if (ret == 0) {
+// 			std::cout << TIMEOUT << " seconds elapsed. No events detected." << std::endl;
+// 			// std::cerr << "ever here?" << std::endl;
+// 			continue;
+// 		}
 
-		if (fds[0].revents & POLLIN) {
-			std::cout << "stdin is readable" << std::endl;
-			break;
-		}
+// 		if (fds[0].revents & POLLIN) {
+// 			std::cout << "stdin is readable" << std::endl;
+// 			break;
+// 		}
 
-		if (fds[1].revents & POLLOUT) {
-			std::cout << "stdout is writable" << std::endl;
-			// sleep(3);
+// 		if (fds[1].revents & POLLOUT) {
+// 			std::cout << "stdout is writable" << std::endl;
+// 			// sleep(3);
 			
-			// close(1);
-			// fds[1].fd = -1;
-		}
-	}
-}
+// 			// close(1);
+// 			// fds[1].fd = -1;
+// 		}
+// 	}
+// }
 
 
-#define MAX_EVENTS 5
-#define READ_SIZE 10
-void	epoll_usage(void)
-{
-	int running = 1, event_count, i;
-	ssize_t bytes_read;
-	char read_buffer[READ_SIZE + 1];
-	struct epoll_event event;
-	struct epoll_event events[MAX_EVENTS];
-	int epoll_fd = epoll_create1(0);
+// #define MAX_EVENTS 5
+// #define READ_SIZE 10
+// void	epoll_usage(void)
+// {
+// 	int running = 1, event_count, i;
+// 	ssize_t bytes_read;
+// 	char read_buffer[READ_SIZE + 1];
+// 	struct epoll_event event;
+// 	struct epoll_event events[MAX_EVENTS];
+// 	int epoll_fd = epoll_create1(0);
 
-	if (epoll_fd == -1) {
-		fprintf(stderr, "Failed to create epoll file descriptor\n");
-		return;
-	}
+// 	if (epoll_fd == -1) {
+// 		fprintf(stderr, "Failed to create epoll file descriptor\n");
+// 		return;
+// 	}
 
-	event.events = EPOLLIN;
-	event.data.fd = STDIN_FILENO;
+// 	event.events = EPOLLIN;
+// 	event.data.fd = STDIN_FILENO;
 
-	if(epoll_ctl(epoll_fd, EPOLL_CTL_ADD, 0, &event))
-	{
-		fprintf(stderr, "Failed to add file descriptor to epoll\n");
-		close(epoll_fd);
-		return;
-	}
+// 	if(epoll_ctl(epoll_fd, EPOLL_CTL_ADD, 0, &event))
+// 	{
+// 		fprintf(stderr, "Failed to add file descriptor to epoll\n");
+// 		close(epoll_fd);
+// 		return;
+// 	}
 
-	while (running) {
-		printf("\nPolling for input...\n");
-		event_count = epoll_wait(epoll_fd, events, MAX_EVENTS, 30000);
-		printf("%d ready events\n", event_count);
-		for (i = 0; i < event_count; i++) {
-			printf("Reading file descriptor '%d' -- ", events[i].data.fd);
-			bytes_read = read(events[i].data.fd, read_buffer, READ_SIZE);
-			printf("%zd bytes read.\n", bytes_read);
-			read_buffer[bytes_read] = '\0';
-			printf("Read '%s'\n", read_buffer);
+// 	while (running) {
+// 		printf("\nPolling for input...\n");
+// 		event_count = epoll_wait(epoll_fd, events, MAX_EVENTS, 30000);
+// 		printf("%d ready events\n", event_count);
+// 		for (i = 0; i < event_count; i++) {
+// 			printf("Reading file descriptor '%d' -- ", events[i].data.fd);
+// 			bytes_read = read(events[i].data.fd, read_buffer, READ_SIZE);
+// 			printf("%zd bytes read.\n", bytes_read);
+// 			read_buffer[bytes_read] = '\0';
+// 			printf("Read '%s'\n", read_buffer);
 		
-			if(!strncmp(read_buffer, "stop\n", 5))
-			running = 0;
-		}
-	}
+// 			if(!strncmp(read_buffer, "stop\n", 5))
+// 			running = 0;
+// 		}
+// 	}
 
-	if (close(epoll_fd)) {
-		fprintf(stderr, "Failed to close epoll file descriptor\n");
-		return;
-	}
+// 	if (close(epoll_fd)) {
+// 		fprintf(stderr, "Failed to close epoll file descriptor\n");
+// 		return;
+// 	}
 
-	puts("end of epoll_usage");
-	return;
-}
+// 	puts("end of epoll_usage");
+// 	return;
+// }
--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,43 @@
 #include <Webserv.hpp>
 
-#include <unistd.h>		// close
+#include <arpa/inet.h>
+#include <cstring>
+#include <errno.h>
+#include <fcntl.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <poll.h>
 #include <stdio.h>
 #include <string.h>
-#include <sys/types.h>
 #include <sys/socket.h>
-#include <netdb.h>
-#include <arpa/inet.h>
-#include <netinet/in.h>
-#include <cstring>
+#include <sys/types.h>
+#include <unistd.h> // close
+#ifdef __linux__
+#include <sys/epoll.h>
+#endif
 
 #include <iostream>
-void	remy_serv(void);
 
+#define RESERVED_FDS 3         // stdin, stdout, stderr
+#define INCOMING_FD_LIMIT 1024 // Makefile, shell(ulimit -n)
+// TODO if FD minimum = 5, immediate turning off webserv with error.
+// 4th is listener FD, 5th FD is client.
+#define FD_LIMIT 1024 - 3
 
-int main() {
-	// poll_usages();
-	// epoll_usage();
-	// getaddrinfo_usage();
-	// server();
-	remy_serv();
-	return 0;
+int              remy_serv(void);
+int              create_listener_socket(void);
+struct addrinfo *get_server_addrinfo(void);
+int              bind_to_socket(struct addrinfo *server);
+static int       make_socket_non_blocking(int sfd);
+
+int main()
+{
+    // poll_usages();
+    // epoll_usage();
+    // getaddrinfo_usage();
+    // server();
+    remy_serv();
+    return 0;
 }
 
 #define PORT "8080"
@@ -30,43 +47,121 @@ int main() {
 // listen();
 // /* accept() goes here */
 
-void	remy_serv(void)
+int remy_serv(void)
 {
-	struct addrinfo	serverSetup;
-	struct addrinfo	*server;
-	int				errHndl;
+    struct pollfd *pfds;
+    int            listener = create_listener_socket();
+    if (listener == -1)
+        return -1;
 
-	std::memset(&serverSetup, 0, sizeof(serverSetup));
-	serverSetup.ai_family = AF_UNSPEC;
-	serverSetup.ai_socktype = SOCK_STREAM;
-	serverSetup.ai_flags = AI_PASSIVE;
-	errHndl = getaddrinfo(NULL, PORT, &serverSetup, &server);
-	if (errHndl != 0) {
-		std::cerr << "Server getaddrinfo: " << gai_strerror(errHndl);
-		return;
-	}
-	
-	struct addrinfo	*p;
-	int				listener;
-	for (p = server; server != NULL; p = p->ai_next) {
-		listener = socket(server->ai_family, server->ai_socktype, server->ai_protocol);
-		if (listener < 0) {
-			continue;
-		}
-		if (bind(listener, p->ai_addr, p->ai_addrlen) == -1) {
-			close(listener);
-			continue;
-		}
-
-		break;
-	}
-	if (p == NULL) {
-		std::cerr << "Server socket: " << strerror(errno);
-		return;
-	}
-	puts("passed");
+    // struct pollfd *pfds = (pollfd *)malloc(sizeof(pollfd) * FD_LIMIT);
+    // if (pfds == NULL)
+    // {
+    //     close(listener);
+    //     return -1;
+    // }
+    // int nfds = 0;
+(void)pfds;
+    puts("yur");
+    if (epoll_create(FD_LIMIT) == -1) // parameter must be bigger than 0, rtfm
+    {
+        std::cerr << "Server listen: " << strerror(errno);
+    	return -1;
+    }
+	return (0);
 }
 
+int create_listener_socket(void)
+{
+    struct addrinfo *server = get_server_addrinfo();
+    if (server == NULL)
+    {
+        return -1;
+    }
+
+    int listener = bind_to_socket(server);
+    freeaddrinfo(server);
+    if (listener == -1)
+    {
+        return -1;
+    }
+    if (make_socket_non_blocking(listener) == -1)
+    {
+        return -1;
+    }
+    // TODO test with 1~5 maximum pending queue of people connecting
+    if (listen(listener, SOMAXCONN) == -1)
+    {
+        std::cerr << "Server listen: " << strerror(errno);
+        return -1;
+    }
+    return listener;
+}
+
+struct addrinfo *get_server_addrinfo(void)
+{
+    struct addrinfo  serverSetup;
+    struct addrinfo *server;
+    int              errHndl;
+
+    std::memset(&serverSetup, 0, sizeof(serverSetup));
+    serverSetup.ai_family = AF_UNSPEC;     // Use IPv4 or IPv6
+    serverSetup.ai_socktype = SOCK_STREAM; // TCP stream sockets
+    serverSetup.ai_flags = AI_PASSIVE;     // Use my IP
+
+    errHndl = getaddrinfo(NULL, PORT, &serverSetup, &server);
+    if (errHndl != 0)
+    {
+        std::cerr << "Server getaddrinfo: " << gai_strerror(errHndl)
+                  << std::endl;
+        return NULL;
+    }
+    return server;
+}
+
+int bind_to_socket(struct addrinfo *server)
+{
+    struct addrinfo *p;
+    int              listener;
+    for (p = server; server != NULL; p = p->ai_next)
+    {
+        listener =
+            socket(server->ai_family, server->ai_socktype, server->ai_protocol);
+        if (listener < 0)
+        {
+            continue;
+        }
+        int reUse = 1;
+        setsockopt(listener, SOL_SOCKET, SO_REUSEADDR, &reUse, sizeof(int));
+        if (bind(listener, p->ai_addr, p->ai_addrlen) == -1)
+        {
+            close(listener);
+            continue;
+        }
+        return listener;
+    }
+    std::cerr << "Server bind_to_socket: " << strerror(errno);
+    return -1;
+}
+
+static int make_socket_non_blocking(int sfd)
+{
+    int currentFlags = fcntl(sfd, F_GETFL, 0);
+    if (currentFlags == -1)
+    {
+        std::cerr << "fcntl: " << strerror(errno);
+        return -1;
+    }
+
+    currentFlags |= O_NONBLOCK;
+    int fcntlResult = fcntl(sfd, F_SETFL, currentFlags);
+    if (fcntlResult == -1)
+    {
+        std::cerr << "fcntl: " << strerror(errno);
+        return -1;
+    }
+    return 0;
+}
 
 // int nbytes = recv(pfds[i].fd, buf, sizeof buf - 1, 0);
 // if (nbytes > 0) {
